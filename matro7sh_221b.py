fa8f46ef871067cc70d0e7c39639ddf88fb29954
winds0r <wontsleep@protonmail.com>
2023-11-23 18:51:37 +0100

    fix(loaders): splitting symbol names

--- matro7sh_221b.py
+++ matro7sh_221b.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- Coding: UTF-8 -*-
-# Author: @jenaye_fr
+# Author: @jenaye_fr & @djnn1337
 # Created on: Mon, 17. Nov 2023
 # Description: matro7sh 221b Loader support for Havoc C2 framework
 # Usage: Load this script into Havoc: Scripts -> Scripts Manager -> Load to create matro7sh Tab
@@ -13,12 +13,12 @@ import havocui  # type: ignore
 from datetime import datetime
 
 # Configuration
-LOADER_PATH = shutil.which("baker")
+BAKER_LOADER_PATH = shutil.which("221b")
 BAKER_ENCRYPT_TECHNIQUES = ["xor", "chacha20", "aes"]
 
 # Variables & Defaults
 baker_shellcode_path = ""
-baker_shellcode_encryption_key = ""
+baker_shellcode_encryption_key = "0123456789ABCDEF1123345611111111"
 baker_shellcode_encryption_kind = "xor"
 
 # Colors
@@ -30,53 +30,52 @@ HAVOC_INFO = "#8be9fd"  # Cyan
 HAVOC_WARNING = "#ffb86c"  # Orange
 
 # Labels
-label_to_replace = f"<b style=\"color:{HAVOC_ERROR};\">No shellcode selected.</b>"
+baker_label_to_replace = f"<b style=\"color:{HAVOC_ERROR};\">No shellcode selected.</b>"
 
-if not LOADER_PATH:
+if not BAKER_LOADER_PATH:
     print("[-] Loader not found in $PATH")
     print("Please run script located in install/ directory :)")
-    havocui.messagebox("Loader not found in: ", LOADER_PATH)
 
 # Create dialog and log widget
 dialog = havocui.Dialog("Matro7sh 221b Payload Generator", True, 670, 400)
 log = havocui.Logger("matro7sh baker Log")
 
 
-def change_shellcode_encrypt_method(num):
+def baker_change_shellcode_encrypt_method(num):
     global baker_shellcode_encryption_kind
     if num:
         baker_shellcode_encryption_kind = BAKER_ENCRYPT_TECHNIQUES[num - 1]
     else:
-        baker_shellcode_encryption_kind = "CRT"
+        baker_shellcode_encryption_kind = "xor"
     print("[*] Shellcode execution method changed: ", baker_shellcode_encryption_kind)
 
 
-def change_default_key_baker(k):
+def baker_change_default_key(k):
     global baker_shellcode_encryption_key
     baker_shellcode_encryption_key = k
     print("[*] Key changed: ", baker_shellcode_encryption_key)
 
 
-def change_shellcode_path():
+def baker_change_shellcode_path():
     global baker_shellcode_path
-    global label_to_replace
+    global baker_label_to_replace
 
     baker_shellcode_path = havocui.openfiledialog("Shellcode path").decode("ascii")
     print("[*] Shellcode path changed: ", baker_shellcode_path, ".")
 
     formatted_shellcode_path = f"<span style=\"color:{HAVOC_SUCCESS};\">{baker_shellcode_path}</span>"
-    dialog.replaceLabel(label_to_replace, formatted_shellcode_path)
-    label_to_replace = formatted_shellcode_path if baker_shellcode_path != " " else f"<b style=\"color:{HAVOC_ERROR};\">No shellcode selected.</b>"
+    dialog.replaceLabel(baker_label_to_replace, formatted_shellcode_path)
+    baker_label_to_replace = formatted_shellcode_path if baker_shellcode_path != " " else f"<b style=\"color:{HAVOC_ERROR};\">No shellcode selected.</b>"
 
 
 # Generate payload
-def run():
+def baker_run():
     def get_build_command() -> str:
         global baker_shellcode_path
         global baker_shellcode_encryption_key
         global baker_shellcode_encryption_kind
 
-        base_cmd = f'{LOADER_PATH} bake'
+        base_cmd = f'{BAKER_LOADER_PATH} bake'
         if baker_shellcode_path != "":
             base_cmd = f'{base_cmd} --shellPath {baker_shellcode_path}'
 
@@ -118,30 +117,30 @@ def run():
     dialog.close()
 
 
-def build():
-    dialog.clear()
-
-    # Get Listeners
-    global listeners
-    listeners = havoc.GetListeners()
-
-    # Build Dialog
-    dialog.addLabel(f"<b>──────────────────────────── Required Settings for 221b ─────────────────────────────</b>")
-    dialog.addButton("Choose shellcode", change_shellcode_path)
-    dialog.addLabel(label_to_replace)
-
-    dialog.addLabel("<b>[*] Shellcode encryption method</b>")
-    dialog.addCombobox(change_shellcode_encrypt_method, "CRT", *BAKER_ENCRYPT_TECHNIQUES)
-
-    dialog.addLabel("<b>[*] Encryption key (Default: random)</b>")
-    dialog.addLineedit("e.g. 0123456789ABCDEF1123345611111111", change_default_key_baker)
-
-    dialog.addButton("Generate", run)
-    dialog.exec()
-
-
 def loader_generator():
+    def build():
+        dialog.clear()
+
+        # Get Listeners
+        global listeners
+        listeners = havoc.GetListeners()
+
+        # Build Dialog
+        dialog.addLabel(f"<b>──────────────────────────── Required Settings for 221b ─────────────────────────────</b>")
+        dialog.addButton("Choose shellcode", baker_change_shellcode_path)
+        dialog.addLabel(baker_label_to_replace)
+
+        dialog.addLabel("<b>[*] Shellcode encryption method</b>")
+        dialog.addCombobox(baker_change_shellcode_encrypt_method, "xor", *BAKER_ENCRYPT_TECHNIQUES)
+
+        dialog.addLabel("<b>[*] Encryption key (Default: random)</b>")
+        dialog.addLineedit("e.g. 0123456789ABCDEF1123345611111111", baker_change_default_key)
+
+        dialog.addButton("Generate", baker_run)
+        dialog.exec()
+
     build()
+
 
 # Create Tab
 havocui.createtab("Matro7sh 221b", "221b loader", loader_generator)
--- matro7sh_myph.py
+++ matro7sh_myph.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- Coding: UTF-8 -*-
-# Author: @jenaye_fr
+# Author: @jenaye_fr & @djnn1337
 # Created on: Mon, 17. Nov 2023
 # Description: matro7sh myph Loader support for Havoc C2 framework
 # Usage: Load this script into Havoc: Scripts -> Scripts Manager -> Load to create matro7sh Tab
@@ -12,9 +12,8 @@ import havocui  # type: ignore
 from datetime import datetime
 
 # Configuration
-LOADER_PATH = shutil.which("myph")
+MYPH_LOADER_PATH = shutil.which("myph")
 MYPH_EXEC_TECHNIQUES = ["CRT", "ProcessHollowing", "CreateThread", "Syscall"]
-
 
 # Variables & Defaults
 myph_shellcode_path = ""
@@ -32,21 +31,19 @@ HAVOC_INFO = "#8be9fd"  # Cyan
 HAVOC_WARNING = "#ffb86c"  # Orange
 
 # Labels
-label_to_replace = f"<b style=\"color:{HAVOC_ERROR};\">No shellcode selected.</b>"
+myph_label_to_replace = f"<b style=\"color:{HAVOC_ERROR};\">No shellcode selected.</b>"
 
-
-if not LOADER_PATH:
+if not MYPH_LOADER_PATH:
     print("[-] Loader not found in $PATH")
     print("Please run script located in install/ directory :)")
-    havocui.messagebox("Loader not found in: ", LOADER_PATH)
+    havocui.messagebox("Loader not found in: ", MYPH_LOADER_PATH)
 
 # Create dialog and log widget
 dialog = havocui.Dialog("Matro7sh Myph Payload Generator", True, 670, 400)
 log = havocui.Logger("matro7sh myph Log")
 
 
-
-def change_shellcode_exec_method(num):
+def myph_change_shellcode_exec_method(num):
     global myph_shellcode_execution_technique
     if num:
         myph_shellcode_execution_technique = MYPH_EXEC_TECHNIQUES[num - 1]
@@ -55,32 +52,33 @@ def change_shellcode_exec_method(num):
     print("[*] Shellcode execution method changed: ", myph_shellcode_execution_technique)
 
 
-def change_target_process(p):
+def myph_change_target_process(p):
     global myph_target_process
     myph_target_process = p
     print("[*] Target process: ", myph_target_process)
 
 
-def change_default_key(k):
+def myph_change_default_key(k):
     global myph_shellcode_encryption_key
     myph_shellcode_encryption_key = k
     print("[*] Key changed: ", myph_shellcode_encryption_key)
 
 
-def change_shellcode_path():
+def myph_change_shellcode_path():
     global myph_shellcode_path
-    global label_to_replace
+    global myph_label_to_replace
 
     myph_shellcode_path = havocui.openfiledialog("Shellcode path").decode("ascii")
     print("[*] Shellcode path changed: ", myph_shellcode_path, ".")
 
     formatted_shellcode_path = f"<span style=\"color:{HAVOC_SUCCESS};\">{myph_shellcode_path}</span>"
-    dialog.replaceLabel(label_to_replace, formatted_shellcode_path)
-    label_to_replace = formatted_shellcode_path if myph_shellcode_path != " " else f"<b style=\"color:{HAVOC_ERROR};\">No shellcode selected.</b>"
+    dialog.replaceLabel(myph_label_to_replace, formatted_shellcode_path)
+    myph_label_to_replace = formatted_shellcode_path if myph_shellcode_path != " " \
+        else f"<b style=\"color:{HAVOC_ERROR};\">No shellcode selected.</b>"
 
 
 # Generate payload
-def run():
+def myph_run():
     def get_build_command() -> str:
         global myph_shellcode_path
         global myph_shellcode_encryption_key
@@ -88,7 +86,7 @@ def run():
         global myph_target_process
         global myph_shellcode_encryption_kind
 
-        base_cmd = f'{LOADER_PATH}'
+        base_cmd = f'{MYPH_LOADER_PATH}'
         if myph_shellcode_path != "":
             base_cmd = f'{base_cmd} --shellcode {myph_shellcode_path}'
 
@@ -136,34 +134,33 @@ def run():
     dialog.close()
 
 
-def build():
-    dialog.clear()
+def myph_loader_generator():
+    def build():
+        dialog.clear()
 
-    # Get Listeners
-    global listeners
-    listeners = havoc.GetListeners()
+        # Get Listeners
+        global listeners
+        listeners = havoc.GetListeners()
 
-    # Build Dialog
-    dialog.addLabel(f"<b>──────────────────────────── Required Settings for Myph ─────────────────────────────</b>")
-    dialog.addButton("Choose shellcode", change_shellcode_path)
-    dialog.addLabel(label_to_replace)
+        # Build Dialog
+        dialog.addLabel(f"<b>──────────────────────────── Required Settings for Myph ─────────────────────────────</b>")
+        dialog.addButton("Choose shellcode", myph_change_shellcode_path)
+        dialog.addLabel(myph_label_to_replace)
 
-    dialog.addLabel("<b>[*] Shellcode execution method</b>")
-    dialog.addCombobox(change_shellcode_exec_method, "CRT", *MYPH_EXEC_TECHNIQUES)
+        dialog.addLabel("<b>[*] Shellcode execution method</b>")
+        dialog.addCombobox(myph_change_shellcode_exec_method, "CRT", *MYPH_EXEC_TECHNIQUES)
 
-    dialog.addLabel("<b>[*] AES Encryption key (Default: random)</b>")
-    dialog.addLineedit("e.g. 0123456789ABCDEF1123345611111111", change_default_key)
+        dialog.addLabel("<b>[*] AES Encryption key (Default: random)</b>")
+        dialog.addLineedit("e.g. 0123456789ABCDEF1123345611111111", myph_change_default_key)
 
-    dialog.addLabel("<b>[*] Process to inject to (Default: cmd.exe)</b>")
-    dialog.addLineedit("e.g. teams.exe", change_target_process)
+        dialog.addLabel("<b>[*] Process to inject to (Default: cmd.exe)</b>")
+        dialog.addLineedit("e.g. teams.exe", myph_change_target_process)
 
-    dialog.addButton("Generate", run)
-    dialog.exec()
+        dialog.addButton("Generate", myph_run)
+        dialog.exec()
 
-
-def loader_generator():
     build()
 
 
 # Create Tab
-havocui.createtab("Matro7sh myph", "myph loader", loader_generator)
+havocui.createtab("Matro7sh myph", "myph loader", myph_loader_generator)
